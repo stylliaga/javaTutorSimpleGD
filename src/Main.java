89b2b9cc777d6126994fd94b3776226cb8e67033
stylliaga <stylliaga@yandex.ru>
2019-03-26 14:07:15 +0300

    Урок1. Переменные и типы данных.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        // byte typ  - from -128 to 127
+        // try it
+        byte byt = -128;
+        System.out.println("Byte - "+ byt);
+
+        // int - from -2^31-1 to 2^31
+        int nt = 999999999;
+        System.out.println("Int - " + nt);
+
+        
+    }
+}
