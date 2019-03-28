ac0eab673fb1d0cab281b2cafe3b4bbbb49fe503
stylliaga <stylliaga@yandex.ru>
2019-03-28 15:01:55 +0300

    Консоль: читаем и выводим то что пишщет юзер

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,11 @@
 import java.lang.String;
-
+import  java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        // byte typ  - from -128 to 127
-        // try it
-        byte byt = -128;
-        System.out.println("Byte - "+ byt);
-
-        // int - from -2^31-1 to 2^31
-        int nt = 999999999;
-        System.out.println("Int - " + nt);
-
-        // lng это -2в63
-        // при этом в 32 битной системе длина равно int
-        // а в 64 битной принимает уже 64
-        long lng = 1000000001;
-        System.out.println("Long - " + lng);
-
-        //важные моменты в работе с double и float
-        // при написании, после присвоенных значений добавлять соответствующие буквы.
-        float flt = 12.33f;
-        double dbl = 234.45645234d;
-
-        // типы дныых "ложь - истина"
-        boolean bln = true;
-        boolean bln2 = false;
-
-        // строковые переменные
-        // для работы с ними необходимо подключить дополнительную библиотеку
-        // import java.lang.String и писать седует с ДВОЙНЫМИ КОВЫЧКАМИ
-        String str = "Что по поводу строковых переменных, то здесь можно вводить, в принципе, любое количество символов";
-        System.out.println("Переменная str - " + str);
-
-        // переменная содержащая один символ инициализируется С ОДИНОЧНЫМИ КОВЫЧКАМИ - так
-        char chr = 'w';
-        System.out.println("Выводим переменную char - " + chr);
-
-
+        // получаем данные от юзера
+        // подключаем класс
+        Scanner str = new Scanner(System.in);
+        System.out.println("Your string is: " + str.nextLine());
     }
 }
