04ac10614ad8ed9f365d5b155802565efe17c9d5
stylliaga <stylliaga@yandex.ru>
2019-03-28 14:43:58 +0300

    Пробежались по переменным. Дописаны char,string,boolean

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.lang.String;
+
+
 public class Main {
     public static void main(String[] args){
         // byte typ  - from -128 to 127
@@ -19,6 +22,21 @@ public class Main {
         // при написании, после присвоенных значений добавлять соответствующие буквы.
         float flt = 12.33f;
         double dbl = 234.45645234d;
-        
+
+        // типы дныых "ложь - истина"
+        boolean bln = true;
+        boolean bln2 = false;
+
+        // строковые переменные
+        // для работы с ними необходимо подключить дополнительную библиотеку
+        // import java.lang.String и писать седует с ДВОЙНЫМИ КОВЫЧКАМИ
+        String str = "Что по поводу строковых переменных, то здесь можно вводить, в принципе, любое количество символов";
+        System.out.println("Переменная str - " + str);
+
+        // переменная содержащая один символ инициализируется С ОДИНОЧНЫМИ КОВЫЧКАМИ - так
+        char chr = 'w';
+        System.out.println("Выводим переменную char - " + chr);
+
+
     }
 }
