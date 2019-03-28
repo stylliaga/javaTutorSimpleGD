980ddd06b21baa94553c5b11cbf9c1aa527a8b12
stylliaga <stylliaga@yandex.ru>
2019-03-28 15:28:54 +0300

    Консоль: математические операции с простыми числами\n которые вводит юзер

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,47 @@ import  java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        // получаем данные от юзера
-        // подключаем класс
-        Scanner str = new Scanner(System.in);
-        System.out.println("Your string is: " + str.nextLine());
+        // математические операции
+
+        // обьявляем новый сканер и пихаем его в переменную
+        Scanner scnr = new Scanner(System.in);
+
+        // обьявляем переменные
+        int a,b,result;
+
+        // говорим юзеру что что то ввел
+        System.out.println("Операция сложения");
+        System.out.print("Введите первое число:");
+        a = scnr.nextInt();
+        System.out.print("Введите второе число:");
+        b = scnr.nextInt();
+        result = a + b;
+        System.out.println("Сумма ваших чисел равна: " + result);
+        System.out.println("------------------------------------------------------");
+        System.out.println("Операция вычитания");
+        System.out.print("Введите первое число:");
+        a = scnr.nextInt();
+        System.out.print("Введите второе число:");
+        b = scnr.nextInt();
+        result = a - b;
+        System.out.println("Разность ваших чисел равна: " + result);
+        System.out.println("------------------------------------------------------");
+        System.out.println("Операция умножения");
+        System.out.print("Введите первое число:");
+        a = scnr.nextInt();
+        System.out.print("Введите второе число:");
+        b = scnr.nextInt();
+        result = a * b;
+        System.out.println("Произведение ваших чисел равно: " + result);
+        System.out.println("------------------------------------------------------");
+        System.out.println("Операция деления");
+        System.out.print("Введите первое число:");
+        a = scnr.nextInt();
+        System.out.print("Введите второе число:");
+        b = scnr.nextInt();
+        result = a / b;
+        System.out.println("Частное ваших чисел равно: " + result);
+        System.out.println("------------------------------------------------------");
+
     }
 }
