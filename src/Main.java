64547a0aae83eed9a56ba09362eb8c0f83e1fa11
stylliaga <stylliaga@yandex.ru>
2019-03-31 21:03:01 +0300

    Консоль:MULTIDIMENS.ARRAYS.STRING.перенос.строк

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,51 @@ import java.lang.String;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        // МАССИВЫ одномерные - юзер указывает размерность массива
-        // затем вводит числа, которые помещаем в массив
-        // затем выводим весь массив
+    public static void main(String[] args) {
+        // МАССИВЫ многомерные - юзер указывает размерность массива
+        // потом вводит строки в каждую ячейку согласно его размерности
 
-        // обьявляем массив и переменную, все простые числа
-        int[] a;
-        int n;
+        // сначала попробуем сделать строковый квадратный многомерный массив
+        Scanner scnr = new Scanner(System.in);
 
-        // создаем сканер и просим чтобы юзер ввел размерность массива
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Пожалуйста, введите желаемое количество ячеек в массиве: ");
-        n = sc.nextInt();
-        a = new int[n];
-        // в цикле записываем в массив значения, которые вводит юзер
-        for(int i = 0; i < n; i++){
-            System.out.println("Введите число, которое помести в ячейкц массива a["+i+"}:");
-            a[i] = sc.nextInt();
+        //собираем данные - сколько ячеек будет в массиве
+        System.out.println("Введите количество ячеек в массиве:");
+
+        //первый эл-т строкового массива
+        int elemStrArr1 = scnr.nextInt();
+
+
+        //собираем данные - сколько ячеек будет в массиве
+        System.out.println("Введите количество подъячеек в массиве:");
+
+        //первый эл-т строкового массива
+        int elemStrArr2 = scnr.nextInt();
+
+        String[][] arrMass = new String[elemStrArr1][elemStrArr2];
+        int f = arrMass.length;
+        // добавляем считывание строки, оптому что программа считывает символ \n после нажатия ЕНТЕР как новую строку
+        scnr.nextLine();
+        for(int i = 0; i < arrMass.length; i++){
+            System.out.println("Введите значение для ячейки массива:" + f );
+            for(int j = 0; j < arrMass[i].length; j++){
+                String strng = new String();
+               // scnr.nextLine();
+                System.out.println("Подъячейка№:" + j );
+                strng = scnr.nextLine();
+                arrMass[i][j] = strng;
+            }
+            f = f-1;
         }
+        // выводим все
+        System.out.println("#################################");
 
-        // выводим полученный массив
-        System.out.println("В массиве \"а\" из "+ a.length +" ячеек содержатся следующие числа:");
-        for(int i = 0; i < a.length;i++){
-            System.out.print(a[i]+ " ");
-       }
+        for(int i = 0; i < arrMass.length; i++){
+            System.out.println("arrMass["+i+"]");
+            for(int j = 0; j < arrMass[i].length; j++){
+                System.out.print(""+arrMass[i][j]+" ");
+            }
+            System.out.println("");
+
+        }
     }
 }
